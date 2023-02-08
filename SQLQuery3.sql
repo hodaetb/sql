ed9d20b8473eb8e1241af4cc37619839df6ca8cc
hodaetb <124813848+hodaetb@users.noreply.github.com>
2023-02-08 21:44:55 +0330

    Add files via upload

---
+++ SQLQuery3.sql
@@ -0,0 +1,34 @@
+﻿--سوال1
+SELECT SUM(Quantity)
+FROM [dbo].[Sale_Table];
+
+--سوال2
+select count (distinct Customer) as Number_of_customrs from [dbo].[Sale_Table];
+
+--سوال3ه
+SELECT Product , SUM(Quantity) as count
+ FROM [dbo].[Sale_Table]
+ GROUP BY Product;
+
+--سوال 4
+Select Customer, SUM(Quantity * UnitPrice) as Total, COUNT(distinct OrderID) as order_count, SUM(Quantity) Item_coount
+from [dbo].[Sale_Table]
+where Quantity * UnitPrice > 1500
+group by Customer 
+
+ --5سوال 
+ 
+Select SUM(UnitPrice * ProfitRatio)
+from [dbo].[Sale_Table] 
+join [dbo].[Sale_Profit] 
+on [dbo].[Sale_Table].Product = [dbo].[Sale_Profit].Product
+
+
+
+--سوال 6
+
+select SUM(count)
+from(
+select Date , count(distinct Customer) as count
+from [dbo].[Sale_Table]
+group by Date) as t
