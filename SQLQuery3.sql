2796914047d57faea4bce5101d0bc40a97bb6fde
hodaetb <124813848+hodaetb@users.noreply.github.com>
2023-02-08 21:47:44 +0330

    Update SQLQuery3.sql

--- SQLQuery3.sql
+++ SQLQuery3.sql
@@ -1,23 +1,23 @@
-﻿--سوال1
+﻿--1
 SELECT SUM(Quantity)
-FROM [dbo].[Sale_Table];
+FROM [dbo].[Sale_Table]
 
---سوال2
-select count (distinct Customer) as Number_of_customrs from [dbo].[Sale_Table];
+--2
+select count (distinct Customer) as Number_of_customrs 
+from [dbo].[Sale_Table]
 
---سوال3ه
+--3
 SELECT Product , SUM(Quantity) as count
- FROM [dbo].[Sale_Table]
- GROUP BY Product;
+FROM [dbo].[Sale_Table]
+GROUP BY Product
 
---سوال 4
+--4
 Select Customer, SUM(Quantity * UnitPrice) as Total, COUNT(distinct OrderID) as order_count, SUM(Quantity) Item_coount
 from [dbo].[Sale_Table]
 where Quantity * UnitPrice > 1500
 group by Customer 
 
- --5سوال 
- 
+ --5
 Select SUM(UnitPrice * ProfitRatio)
 from [dbo].[Sale_Table] 
 join [dbo].[Sale_Profit] 
@@ -25,8 +25,7 @@ on [dbo].[Sale_Table].Product = [dbo].[Sale_Profit].Product
 
 
 
---سوال 6
-
+--6
 select SUM(count)
 from(
 select Date , count(distinct Customer) as count
